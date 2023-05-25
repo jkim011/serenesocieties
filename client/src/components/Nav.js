9b336c6c1a3050584714ea29d85ae4785075d1a8
RyStreet <ryanstreet122@gmail.com>
2023-05-25 12:43:53 -0500

    absolute positioning on navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
+  
   Button,
   Checkbox,
   Grid,
@@ -24,73 +25,37 @@ function NavBar() {
   const navigate = useNavigate();
   console.log(window.location.pathname);
   const [visible, setVisible] = useState(false)
+  
   if(window.location.pathname === '/') {
     navigate(0);
     return null
   } else {
-    // return (
-    //   <Navbar className='nav' collapseOnSelect expand="lg" bg="" style={{marginBottom:"50px"}}>
-    //   <Navbar.Brand as={Link} to="/" className='nav-logo'><img src={LogoLong} className="logo-long " size='small' alt='logo'/></Navbar.Brand>
-    //       <Navbar.Toggle aria-controls="responsive-navbar-nav" className=''/>
-    //       <Navbar.Collapse id="responsive-navbar-nav">
-    //       <Nav className="me-auto main-nav-items">
-    //         <Nav.Link className='' as={Link} to="/shop">Shop</Nav.Link>
-    //         <Nav.Link className='' as={Link} to="/gallery">Gallery</Nav.Link>
-    //         <Nav.Link className='' as={Link} to="/lookbook">Lookbook</Nav.Link>
-    //       </Nav>   
-    //       </Navbar.Collapse>
-    //       <Nav>
-    //         <Nav.Link className='' as={Link} to="/cart">Cart</Nav.Link>
-    //       </Nav>
-    //   </Navbar>
-    // )
+    return (
+      <Navbar className='nav' collapseOnSelect expand="lg" bg="" style={{marginBottom:"50px"}}>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className=''/>
 
-  return (
-    <Grid columns={1}>
-      <Grid.Column>
-        <Button
-          onClick={setVisible}
-          onChange={(e, data) => setVisible(data.checked)}
-        >
-          Nav
-        </Button>
-      </Grid.Column>
+        
+        
+        <Nav>
+        <Navbar.Brand as={Link} to="/" className='nav-logo'><img src={LogoLong} className="logo-long " size='small' alt='logo'/></Navbar.Brand>
+        <Nav.Link as={Link} id='cart' to="/cart">Cart</Nav.Link>
 
-      <Grid.Column>
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            icon='labeled'
-            inverted
-            onHide={() => setVisible(false)}
-            vertical
-            visible={visible}
-            width='thin'
-          >
-            <Menu.Item as='a'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='gamepad' />
-              Games
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='camera' />
-              Channels
-            </Menu.Item>
-          </Sidebar>
+          
+        </Nav>
 
-          <Sidebar.Pusher dimmed={visible}>
-            <Segment basic>
-              <Header as='h3'>Application Content</Header>
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Grid.Column>
-    </Grid>
-  )
+          <Navbar.Collapse  id="responsive-navbar-nav">
+            <Nav className="me-auto main-nav-items">
+
+              <Nav.Link className='' as={Link} to="/shop">Shop</Nav.Link>
+              <Nav.Link className='' as={Link} to="/gallery">Gallery</Nav.Link>
+              <Nav.Link className='' as={Link} to="/lookbook">Lookbook</Nav.Link>
+
+            </Nav>   
+          </Navbar.Collapse>
+
+
+      </Navbar>
+    )
   }
 }
 
