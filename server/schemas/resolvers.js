9e9a6c1ec5740a474ff2a4238d577f5147cf126c
RyStreet <ryanstreet122@gmail.com>
2023-05-31 12:28:49 -0500

    resolvers and typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,18 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Project } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+module.exports = resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user){
+                return User.findOne({_id: context.user._id})
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    },
+    users: async () => {
+        return User.find();
+      },
+}
