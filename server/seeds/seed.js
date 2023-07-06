06e37de50e3ce499cd668677bbef7b9e9f772436
Jared Kim <jaredkim01@yahoo.com>
2023-07-06 12:34:07 -0500

    added hoodie to seeds

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -7,7 +7,9 @@ db.once('open', async () => {
   const categories = await Category.insertMany([
     {name: "All Products", routeName: "all-products"},
     {name: "Tees", routeName: "tees"},
-    {name: "Posters", routeName: "posters"}
+    {name: "Hoodies", routeName: "hoodies"},
+    {name: "Posters", routeName: "posters"},
+    {name: "Halloween Special", routeName: "halloween-special"}
   ]);
   console.log("categories seeded");
 
@@ -16,6 +18,8 @@ db.once('open', async () => {
     {size: "S", quantity: 10},
     {size: "M", quantity: 8},
     {size: "L", quantity: 12},
+    {size: "M", quantity: 5},
+    {size: "L", quantity: 8},
     {size: "10x12 in", quantity: 15},
     {size: "13x15 in", quantity: 8}
   ]);
@@ -33,12 +37,21 @@ db.once('open', async () => {
       inventory: [inventory[0]._id, inventory[1]._id, inventory[2]._id]
     },
     {
+      name: "Seeded hoodie",
+      image: "../../images/clothes/halloween-hoodie.jpg",
+      image2: "../../images/clothes/halloween-hoodie-model.jpg",
+      description: "testing seeded data hoodie",
+      price: 50.00,
+      categories: [categories[2]._id, categories[4]._id],
+      inventory: [inventory[3]._id, inventory[4]._id]
+    },
+    {
       name: "Seeded poster",
       image: "../../images/posters/toxicPalmtree.jpg",
       description: "testing seeded data poster",
       price: 10.00,
-      categories: [categories[2]._id],
-      inventory: [inventory[3]._id, inventory[4]._id]
+      categories: [categories[3]._id],
+      inventory: [inventory[5]._id, inventory[6]._id]
     }
   ]);
   console.log("products seeded");
