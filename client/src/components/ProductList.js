06f6261f8624600e28df952df2c6e42601bba4b5
Jared Kim <jaredkim01@yahoo.com>
2023-07-06 14:33:42 -0500

    code cleanup, seeds added

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -3,44 +3,95 @@ import { useState } from "react";
 import { Link, useParams } from 'react-router-dom';
 import shirt from "../assets/clothes/testShirt.png";
 import "../styles/productList.css";
-import { QUERY_CATEGORIES } from '../utils/queries';
+import { QUERY_CATEGORIES, QUERY_PRODUCTS } from '../utils/queries';
 import { useQuery } from "@apollo/client";
 
-const ProductList = ({products}) => {
-  const { categoryId } = useParams();
-  const {loading, data, error} = useQuery(QUERY_CATEGORIES)
-  console.log(error)
-  const categories = data?.categories || []
+const ProductList = () => {
+  const { loading, data, error } = useQuery(QUERY_PRODUCTS);
+  const products = data?.products || [];
+
+  const params = useParams()
   
-  // let currentCategory = category.routeName
-  // console.log(currentCategory, "current category")
+  var selectProductArr = [] 
 
-  // const [ route, setRoute ] = useState(categories.category.routeName)
+  const matchingCategories = () =>{
+    for (let i = 0; i < products.length; i++) {
+      for(let j=0; j<products[i].categories.length; j++){
+        let productChose = products[i]
+        let productCategory = products[i].categories[j].routeName
+        if(productCategory == params.routeName){
+          console.log(productCategory,"match", productChose)
+          selectProductArr.push(productChose)
+        
+        } 
+      }
+    }
+  }
 
+  matchingCategories()
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
   return(
-    <div></div>
-    // <div id="productCardContainer" className="d-flex flex-row flex-wrap justify-content-around">
-    //   {products && 
-    //     products.map((product) => (
-    //       <div key={product._id} id="productCard" className="m-2">
+    <div
+      id="productCardContainer"
+      className="d-flex flex-row flex-wrap justify-content-around"
+    >
+      {params.routeName == "all-products" ? products && products.map((product) =>(
 
-    //         <div id="productHead">
-    //           <Link to={`/shop/${categories.routeName}/${product._id}`}>
-    //             <img className="productImg" src={product.image} alt="" />
-    //             <img className="productImg productImg2" src={product.image2} alt="" />
-    //           </Link>
-    //         </div>
+      <div key={product._id} id="productCard" className="m-2">
+      <div id="productHead">
+        <Link to={`/shop/products/${product._id}`}>
+          <img className="productImg" src={product.image} alt="" />
+          <img
+            className="productImg productImg2"
+            src={product.image2}
+            alt=""
+          />
+        </Link>
+      </div>
 
-    //         <div className="container ">
-    //           <div id="productDetails" className="column">
-    //             <h5 className="col text-center productText">{product.name}</h5>
-    //             <h5 className="col text-center productText">${product.price}</h5>
-    //           </div>
-    //         </div>
+      <div className="container ">
+        <div id="productDetails" className="column">
+          <h5 className="col text-center productText">
+            {product.name}
+          </h5>
+          <h5 className="col text-center productText">
+            ${product.price}
+          </h5>
+        </div>
+      </div>
+      </div>
+      )) : <></> }
 
-    //       </div>
-    //     ))} 
-    // </div>
+      {selectProductArr.map(product => (
+          <div key={product._id} id="productCard" className="m-2">
+            <div id="productHead">
+              <Link to={`/shop/products/${product._id}`}>
+                <img className="productImg" src={product.image} alt="" />
+                <img
+                  className="productImg productImg2"
+                  src={product.image2}
+                  alt=""
+                />
+              </Link>
+            </div>
+
+            <div className="container ">
+              <div id="productDetails" className="column">
+                <h5 className="col text-center productText">
+                  {product.name}
+                </h5>
+                <h5 className="col text-center productText">
+                  ${product.price}
+                </h5>
+              </div>
+            </div>
+          </div>
+        ))}
+
+    </div>
   )
 }
 
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -9,7 +9,8 @@ db.once('open', async () => {
     {name: "Tees", routeName: "tees"},
     {name: "Hoodies", routeName: "hoodies"},
     {name: "Posters", routeName: "posters"},
-    {name: "Halloween Special", routeName: "halloween-special"}
+    {name: "Halloween Special", routeName: "halloween-special"},
+    {name: "Summer Special", routeName: "summer-special"}
   ]);
   console.log("categories seeded");
 
@@ -52,6 +53,67 @@ db.once('open', async () => {
       price: 10.00,
       categories: [categories[3]._id],
       inventory: [inventory[5]._id, inventory[6]._id]
+    },
+    {
+      name: "Seeded t-shirt 2",
+      image: "../../images/clothes/testShirtBack.jpg",
+      image2: "../../images/clothes/testShirtFront.jpg",
+      description: "testing seeded data shirt",
+      price: 20.00,
+      categories: [categories[1]._id],
+      inventory: [inventory[0]._id, inventory[1]._id, inventory[2]._id]
+    },
+    {
+      name: "Seeded t-shirt 3",
+      image: "../../images/clothes/testShirtBack.jpg",
+      image2: "../../images/clothes/testShirtFront.jpg",
+      description: "testing seeded data shirt",
+      price: 20.00,
+      categories: [categories[1]._id],
+      inventory: [inventory[0]._id, inventory[1]._id, inventory[2]._id]
+    },
+    {
+      name: "Seeded t-shirt 4",
+      image: "../../images/clothes/testShirtBack.jpg",
+      image2: "../../images/clothes/testShirtFront.jpg",
+      description: "testing seeded data shirt",
+      price: 20.00,
+      categories: [categories[1]._id, categories[4]._id],
+      inventory: [inventory[0]._id, inventory[1]._id, inventory[2]._id]
+    },
+    {
+      name: "Seeded poster 2",
+      image: "../../images/posters/toxicPalmtree.jpg",
+      description: "testing seeded data poster",
+      price: 10.00,
+      categories: [categories[3]._id],
+      inventory: [inventory[5]._id, inventory[6]._id]
+    },
+    {
+      name: "Seeded poster 3",
+      image: "../../images/posters/toxicPalmtree.jpg",
+      description: "testing seeded data poster",
+      price: 10.00,
+      categories: [categories[3]._id, categories[4]._id],
+      inventory: [inventory[5]._id, inventory[6]._id]
+    },
+    {
+      name: "Seeded t-shirt summer ",
+      image: "../../images/clothes/testShirtBack.jpg",
+      image2: "../../images/clothes/testShirtFront.jpg",
+      description: "testing seeded data shirt",
+      price: 20.00,
+      categories: [categories[1]._id, categories[5]._id],
+      inventory: [inventory[0]._id, inventory[1]._id, inventory[2]._id]
+    },
+    {
+      name: "Seeded hoodie summer",
+      image: "../../images/clothes/halloween-hoodie.jpg",
+      image2: "../../images/clothes/halloween-hoodie-model.jpg",
+      description: "testing seeded data hoodie",
+      price: 50.00,
+      categories: [categories[2]._id, categories[5]._id],
+      inventory: [inventory[3]._id, inventory[4]._id]
     }
   ]);
   console.log("products seeded");
