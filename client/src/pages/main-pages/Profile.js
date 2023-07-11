4f504f8915f4d7e6e0d959c49ed0d3d5a210569d
RyStreet <ryanstreet122@gmail.com>
2023-07-11 15:48:00 -0500

    nav styling complete

--- client/src/pages/main-pages/Profile.js
+++ client/src/pages/main-pages/Profile.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Auth from "../../utils/auth"
+import { useQuery } from '@apollo/client';
+import {QUERY_ME} from "../../utils/queries"
+
+
 
 function Profile() {
+  const {loading, data} = useQuery(QUERY_ME);
+  
+  const me = data?.me || []
+  console.log(me)
+  
+  if(!Auth.loggedIn()){
   return (
-    <div>
-      Profile
-    </div>
+    <p>Login to view your Profile!</p>
   )
+}
+return(
+  <section className='mt-4 '>
+  <h1>Welcome Back {me.username}</h1>
+  </section>
+  
+)
 }
 
 
