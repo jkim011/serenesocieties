7424cc85a827a2c7a89da21e07939f322d28be5a
RyStreet <ryanstreet122@gmail.com>
2023-07-17 14:02:43 -0500

    starting to render the cart page and componenets

---
+++ client/src/pages/main-pages/Cart.js
@@ -0,0 +1,13 @@
+function Cart() {
+    return (
+      <div>
+        <div className='page-header'>
+          
+          <h2 className='page-title'>Cart</h2>
+        </div>
+  
+      </div>
+    )
+  }
+
+export default Cart
