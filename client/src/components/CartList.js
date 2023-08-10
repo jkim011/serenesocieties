af085440d5b8f45c4cb9e7999616b2ccc0415873
Jared Kim <jaredkim01@yahoo.com>
2023-08-10 13:47:13 -0700

    cart total function

--- client/src/components/CartList.js
+++ client/src/components/CartList.js
@@ -1,45 +1,38 @@
 import React from "react";
 import { useState } from "react";
-import { QUERY_ME, QUERY_SINGLE_PRODUCT, QUERY_CART_PRODUCTS } from "../utils/queries";
+import { QUERY_ME } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 import ProductList from "./ProductList";
 
 const CartList = () => {
     const {loading, data, error} = useQuery(QUERY_ME);
-
     const cartItems = data?.me.cartItems || []
-
     console.log(cartItems, "cartItems")
     
+    // To calculate cart total
+    let cartTotalPrice = 0
+    for ( let i=0; i < cartItems.length; i++) {
+        console.log(cartItems[i].cartProductPrice, "forloop")
+        cartTotalPrice += parseInt(cartItems[i].cartProductPrice)
+    }
+    console.log(cartTotalPrice)
 
-    // let productIds = cartItems.map(cartItem => {
-    //     return cartItem.cartProductId
-    // })
-    // console.log(productIds, "productIDs")
-    
-    // const {loading: productLoading, data: productData, error: productError} = useQuery(QUERY_CART_PRODUCTS, {
-    //     variables: {productId: productIds}
-    // })
-    
-    // console.log(productData, 'productData')
-    
+    return (
+      <div  >
+        {cartItems && cartItems.map((cartItem) =>(
+          <div key={cartItem._id}>
+            <p>Product ID: {cartItem.cartProductId}</p>
+            <p>{cartItem.cartProductName}</p>
+            <p>Stock ID: {cartItem.cartProductSizeId}</p>
+            <p>{cartItem.cartProductSize}</p>
+            <p>${cartItem.cartProductPrice}</p>
+            <img src={cartItem.cartProductImage} style={{width: "50px"}}/>
+          </div>
+        )) } 
 
-    return(
-        <>
-            <div  >
-                {cartItems && cartItems.map((cartItem) =>(
-                    <div key={cartItem._id}>
-                        <p>Product ID: {cartItem.cartProductId}</p>
-                        <p>{cartItem.cartProductName}</p>
-                        <p>Stock ID: {cartItem.cartProductSizeId}</p>
-                        <p>{cartItem.cartProductSize}</p>
-                        <p>${cartItem.cartProductPrice}</p>
-                        <img src={cartItem.cartProductImage} style={{width: "50px"}}/>
+        <p>Total: ${cartTotalPrice}</p>
+      </div>
 
-                    </div>
-                )) } 
-            </div>
-        </>
     )
 }
 
