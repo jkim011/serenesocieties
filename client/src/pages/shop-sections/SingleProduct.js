bbeeec97117b0177dfaa64d332552ce63e1b77ef
RyStreet <ryanstreet122@gmail.com>
2023-08-10 13:49:21 -0500

    working on useState for size select

--- client/src/pages/shop-sections/SingleProduct.js
+++ client/src/pages/shop-sections/SingleProduct.js
@@ -1,22 +1,72 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
+import { useState } from "react";
 import { QUERY_SINGLE_PRODUCT } from "../../utils/queries";
+import {ADD_TO_CART} from "../../utils/mutations"
+import Auth from "../../utils/auth";
 
 function SingleProduct() {
   const { productId } = useParams();
   const { loading, data } = useQuery(QUERY_SINGLE_PRODUCT, {
     variables: { productId: productId }
   });
-  console.log(data, "data")
+  // console.log(data, "data")
   const product = data?.product || {};
   const inventory = product.inventory
-  console.log(product)
-  console.log(inventory, "inventory in SingleProduct")
+  // console.log(product)
+  // console.log(inventory, "inventory in SingleProduct")
+  // console.log(Auth.getProfile().data._id, "userid")
+  // console.log(productId, "product")
+  // console.log(product.image, "image")
+  // console.log(product.price, "price")
 
-  const handleAddToCart = () => {
-    
+  const [size, setSize] = useState("Small")
+  const [sizeId, setSizeId] = useState("")
+  // console.log(product.inventory[0]._id, "size ID")
+
+  const handleSizeSelect = (event) => {
+    const {name, value, key} = event.target;
+   
+
+    if(name === "Size"){
+      setSize(value)
+      
+    }
+    if(name === "sizeId"){
+      setSizeId(key)
+    }
+  
+  }
+  console.log(size)
+  // console.log(sizeId)
+  // const sizeIdValue = document.querySelector(".sizeId")
+  // console.log(sizeIdValue)
+ 
+  
+
+  const [addCartItem, {error}] = useMutation(ADD_TO_CART)
+
+  const handleAddToCart = async (event) => {
+    event.preventDefault();
+
+    try{
+      const {cartData} = await addCartItem({
+        variables:
+        {
+          userId: Auth.getProfile().user._id,
+          cartProductId: productId,
+          cartProductImage: product.image,
+          cartProductPrice: product.price,
+          cartProductSize: size
+          
+        
+        },
+      });
+    } catch(err){
+      console.log(err)
+    }
   }
 
   return (
@@ -50,8 +100,8 @@ function SingleProduct() {
 
         <form className="product-btns">
             <label>Size: </label>
-            <select id="product-size" className="product-size" name="Size">
-              {inventory?.map(stock => <option key={stock._id}>{stock.size}</option>)}
+            <select id="product-size" className="product-size" name="Size" value={size} onChange={handleSizeSelect} >
+              {inventory?.map(stock => <option className="sizeId" key={stock._id}  >{stock.size}</option>)}
             </select>
 
           <button className="btns cart-btn">Add to cart</button>
