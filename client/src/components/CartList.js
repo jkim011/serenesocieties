810f3f06eb04d4ab7d555840787a92e700b6101f
RyStreet <ryanstreet122@gmail.com>
2023-08-16 14:28:45 -0500

    button and state handlers in single product

--- client/src/components/CartList.js
+++ client/src/components/CartList.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { QUERY_ME } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 import ProductList from "./ProductList";
+import  "../styles/cartList.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const CartList = () => {
     const {loading, data, error} = useQuery(QUERY_ME);
@@ -18,16 +21,25 @@ const CartList = () => {
     console.log(cartTotalPrice)
 
     return (
-      <div >
+      <div className="cartContainer container flex " >
+         
         {cartItems && cartItems.map((cartItem) =>(
-          <div key={cartItem._id}>
-            <p>Product ID: {cartItem.cartProductId}</p>
-            <p>{cartItem.cartProductName}</p>
-            <p>Stock ID: {cartItem.cartProductSizeId}</p>
-            <p>{cartItem.cartProductSize}</p>
-            <p>${cartItem.cartProductPrice}</p>
-            <img src={cartItem.cartProductImage} style={{width: "50px"}}/>
+          <div key={cartItem._id} className="border row mb-2" >
+            <div className="col-6">
+              <img className="cartImageComponent" src={cartItem.cartProductImage}/>
+            </div>
+          <div className="col-6 cartItemDetails">
+           
+              <p>{cartItem.cartProductName}</p>
+              
+            
+              <p>{cartItem.cartProductSize}</p>
+             
+              <p>${cartItem.cartProductPrice}</p>
+              </div>
+              <FontAwesomeIcon icon="fa-solid fa-trash-can" />
           </div>
+        
         )) } 
 
         <p>Total: ${cartTotalPrice}</p>
