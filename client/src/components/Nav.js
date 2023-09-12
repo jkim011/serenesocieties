62dca438f2468e92361e9eb6b3e6fd805780381e
Jared Kim <jaredkim01@yahoo.com>
2023-09-11 19:57:24 -0500

    debugged cart count. local cart count still needs pg refresh

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -27,77 +27,60 @@ function NavBar() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  //for local. need to make one for logged in cart
-  // let localCartItems = JSON.parse(localStorage.getItem("allCartItems"))
-  // let cartItemCount = localCartItems.length
+  let localCartItems = JSON.parse(localStorage.getItem("allCartItems"))
+  let cartItems = data?.me.cartItems || []
 
   if(window.location.pathname === '/') {
     return null
   }
   return (
-    // <Navbar className='nav justify-content-between' collapseOnSelect expand="lg" bg="" >
-    //     <Navbar.Brand as={Link} to="/" id='main-nav-brand' className='nav-logo'><img src={LogoLong} className="logo-long " size='small' alt='logo'/></Navbar.Brand>
-    //     <>
-    //       <Button variant='white' className="d-lg-none" onClick={handleShow}>
-    //         <img src={MenuIcon} className='menu-icon'/>
-    //       </Button>
-    //       <Offcanvas show={show} onHide={handleClose} responsive="lg">
-    //         <Offcanvas.Header closeButton/>
-    //         <Offcanvas.Body>
-    //           <Nav className="main-nav-left">   
-    //             <Nav.Link className='main-nav-items' as={Link} to="/shop/all-products" onClick={handleClose} >Shop</Nav.Link>
-    //             {/* <Nav.Link className='main-nav-items' as={Link} to="/gallery" onClick={handleClose} >Gallery</Nav.Link> */}
-    //             <Nav.Link id='lookbook' className='main-nav-items ' as={Link} to="/lookbook" onClick={handleClose} >Lookbook</Nav.Link>
-    //             <Nav.Link id='profile-phone' className='main-nav-items' as={Link} to="/profile/" onClick={handleClose} >Account</Nav.Link>
-    //           </Nav>   
-    //         </Offcanvas.Body>
-    //       </Offcanvas>
-    //       <Nav className='main-nav-right'>
-    //         <Nav.Link id='profile-fullscreen' className='main-nav-items' as={Link} to="/profile/" onClick={handleClose} ><FontAwesomeIcon icon="fa-solid fa-user"></FontAwesomeIcon></Nav.Link>
-    //         <Nav.Link className='main-nav-items' as={Link} id='cart' to="/cart" onClick={handleClose}><FontAwesomeIcon icon="fa-solid fa-cart-shopping"></FontAwesomeIcon></Nav.Link>
-    //       </Nav>
-    //     </>
-    // </Navbar>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <div class=" navbar-collapse justify-content-center justify-content-evenly inline" id="navbarNav">
+        <a className="d-lg-none" onClick={handleShow}>
+          <img src={MenuIcon} className='menu-icon'/>
+        </a>
 
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <div class=" navbar-collapse justify-content-center justify-content-evenly inline" id="navbarNav">
-      <a className="d-lg-none" onClick={handleShow}>
-        <img src={MenuIcon} className='menu-icon'/>
-      </a>
-
-      <Offcanvas show={show} onHide={handleClose} responsive="lg" className="off-canvas-phone">
-        <Offcanvas.Header closeButton/>
-        <Offcanvas.Body >
-          <ul class="navbar-nav nav-width justify-content-start">
-            <li class="nav-item" onClick={handleClose}>
-              <Link class="nav-link" as={Link} to="/shop/all-products">Shop</Link>
+        <Offcanvas show={show} onHide={handleClose} responsive="lg" className="off-canvas-phone">
+          <Offcanvas.Header closeButton/>
+          <Offcanvas.Body >
+            <ul class="navbar-nav nav-width justify-content-start">
+              <li class="nav-item" onClick={handleClose}>
+                <Link class="nav-link" as={Link} to="/shop/all-products">Shop</Link>
+              </li>
+              <li class="nav-item ms-lg-5" onClick={handleClose}>
+                <Link class="nav-link" as={Link} to="/lookbook">Lookbook</Link>
+              </li>
+              {/* <li class="nav-item" onClick={handleClose}>
+                <Link class="nav-link" as={Link} to="/gallery">Gallery</Link>
+              </li> */}
+              <li id='profile-phone' className='nav-item' onClick={handleClose} ><Link class="nav-link" as={Link} to="/profile">Account</Link></li>
+            </ul>
+          </Offcanvas.Body>
+        </Offcanvas>
+        <Link class="navbar-logo nav-logo" as={Link} to="/"><img src={LogoLong} className="logo-long " size='small' alt='logo'/></Link>
+          <ul class="navbar-nav ml-auto nav-width justify-content-end ">
+            <li class="nav-item me-lg-5" id='profile-fullscreen'>
+              <Link class="nav-link" as={Link} to="/profile"><FontAwesomeIcon icon="fa-solid fa-user"></FontAwesomeIcon></Link>
             </li>
-            <li class="nav-item ms-lg-5" onClick={handleClose}>
-              <Link class="nav-link" as={Link} to="/lookbook">Lookbook</Link>
+            <li class="nav-item">
+              <div style={{display:"inline-flex"}}>
+                <Link class="nav-link" as={Link} to="/cart"><FontAwesomeIcon icon="fa-solid fa-cart-shopping"></FontAwesomeIcon></Link>
+                {!Auth.loggedIn() && localCartItems ? (
+                  <div>
+                    {JSON.parse(localStorage.getItem("allCartItems")).length}
+                  </div>
+                ) : (
+                  <div>
+                    {cartItems.length}
+                  </div>
+                )}
+              </div>
             </li>
-            {/* <li class="nav-item" onClick={handleClose}>
-              <Link class="nav-link" as={Link} to="/gallery">Gallery</Link>
-            </li> */}
-            <li id='profile-phone' className='nav-item' onClick={handleClose} ><Link class="nav-link" as={Link} to="/profile">Account</Link></li>
           </ul>
-        </Offcanvas.Body>
-      </Offcanvas>
-      <Link class="navbar-logo nav-logo" as={Link} to="/"><img src={LogoLong} className="logo-long " size='small' alt='logo'/></Link>
-        <ul class="navbar-nav ml-auto nav-width justify-content-end ">
-          <li class="nav-item me-lg-5" id='profile-fullscreen'>
-            <Link class="nav-link" as={Link} to="/profile"><FontAwesomeIcon icon="fa-solid fa-user"></FontAwesomeIcon></Link>
-          </li>
-          <li class="nav-item">
-            <div style={{display:"inline-flex"}}>
-              <Link class="nav-link" as={Link} to="/cart"><FontAwesomeIcon icon="fa-solid fa-cart-shopping"></FontAwesomeIcon></Link>
-              {/* {cartItemCount} */}
-            </div>
-          </li>
-        </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
