ddfbf802a1e49ed83851700e251e13c1def20926
Jared Kim <jaredkim01@yahoo.com>
2024-05-16 23:45:38 -0500

    restyled footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faTiktok, faXTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom';
 import IgIcon from '../assets/icons/ig-icon.png';
 import TiktokIcon from '../assets/icons/tiktok-icon.png';
@@ -15,16 +18,16 @@ function Footer() {
   } else {
   return (
     <div className='footer-container'>
-      <div className='footer-space'>
-        <a href='https://www.instagram.com/serenesocieties/' target="_blank" rel="noreferrer"><img src={IgIcon} className='footer-socials'/></a>
-        <a href='' target="_blank" rel="noreferrer"><img src={TiktokIcon} className='footer-socials'/></a>
-        <a href='' target="_blank" rel="noreferrer"><img src={FacebookIcon} className='footer-socials'/></a>
-        <a href='' target="_blank" rel="noreferrer"><img src={EmailIcon} className='footer-socials'/></a>
-      </div>
-
       <p className='footer-space'>© 2023 Serene</p>
       <a href='/about' className='footer-about footer-space'>About</a>
       <a href='/terms-and-conditions' className='footer-terms footer-space'>Terms & Conditions</a> 
+
+      <div className='footer-space'>
+        <a href='https://www.instagram.com/serenesocieties/' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className='footer-socials'/></a>
+        <a href='' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTiktok} className='footer-socials'/></a>
+        <a href='' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faXTwitter} className='footer-socials'/></a>
+        <a href='' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} className='footer-socials'/></a>
+      </div>
     </div>
   )
 }
